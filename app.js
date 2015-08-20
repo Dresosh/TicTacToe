28207773e53e0ac14fc312367f997629b6b56fe2
Dresosh <contact@dresosh.com>
2015-08-20 14:32:02 -0700

    fixes checkWinner function

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ var $button = $("button")
 var winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[1,4,7],[2,5,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6]];
 
 $boxes.on("click", function(){
-	if($(this).text() === ("")){
-		if(turnCount % 2 === 0){
+	if($(this).text() === ("") ) {
+		if(turnCount % 2 === 0) {
 			$(this).html("X");
 			$(this).addClass("clickedX");
 		} else {
 			$(this).html("O");
 			$(this).addClass("clickedY");
 		}
+		console.log(turnCount)
 		turnCount++;
+		console.log(turnCount)
 		checkWinner(this.innerHTML)
 	} 	
 });
@@ -31,7 +33,7 @@ function resetGame(){
 
 function checkWinner(player){
 	for(var i = 0; i < winConditions.length; i++){
-		if(boxes[winConditions[i][0]].innerHTML === player && boxes[winConditions[i][1]].innerHTML === player && boxes[winConditions[i][2]].innerHTML === player){
+		if($boxes[winConditions[i][0]].innerHTML === player && $boxes[winConditions[i][1]].innerHTML === player && $boxes[winConditions[i][2]].innerHTML === player){
 			$("#message").html("Player " + player + " wins");
 		}
 	}
