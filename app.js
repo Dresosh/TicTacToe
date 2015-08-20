a22cdcac57c52a1415ba0101d37c4619e554856f
Dresosh <contact@dresosh.com>
2015-08-20 03:01:34 -0700

    changing if statements to jQuery format

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var button = document.querySelector("button")
 var winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[1,4,7],[2,5,8],[0,3,6],[1,4,7],[2,5,8]];
 
 $(boxes).on("click", function(){
-	if(!this.innerHTML){
+	if($(!this).html("")){
 		if(turnCount % 2 === 0){
-			this.innerHTML = "X";
+			$(this).html("X");
+			$(this).addClass("clickedX");
 		} else {
-			this.innerHTML = "O";
+			$(this).html("O");
+			$(this).addClass("clickedY");
 		}
 		checkWinner(this.innerHTML)
 		turnCount++;
@@ -16,18 +18,21 @@ $(boxes).on("click", function(){
 });
 
 $(button).on("click", function(){
-	resetBoard();
+	resetGame();
 });
- 
 
-function resetBoard(){
+
+function resetGame(){
 	$(".box").html("");
+	$("#message").html("");
+	$(".box").removeClass("clickedX");
+	$(".box").removeClass("clickedY");
 }
 
 function checkWinner(player){
 	for(var i = 0; i < winConditions.length; i++){
 		if(boxes[winConditions[i][0]].innerHTML === player && boxes[winConditions[i][1]].innerHTML === player && boxes[winConditions[i][2]].innerHTML === player){
-			console.log(player + " wins!");
+			$("#message").html("Player " + player + " wins");
 		}
 	}
 }
